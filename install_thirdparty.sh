6715463056113f4d5d5a06456c3095d03b370b8f
Sebastian <sebastian@sgundersen.com>
2019-09-15 20:22:53 +0200

    add curl to thirdparty install script

--- install_thirdparty.sh
+++ install_thirdparty.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
-sudo apt install unzip lame sox libsox-fmt-mp3
+sudo apt install unzip lame sox libsox-fmt-mp3 curl
